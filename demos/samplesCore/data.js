fc026c6c6675645b7f7616686242cb94fa1725e4
Fred Yang <yangjh02@gmail.com>
2011-01-24 17:59:28 -0500

    add test file

---
+++ demos/samplesCore/data.js
@@ -0,0 +1,252 @@
+[
+	{ Name: "The Red Violin", ReleaseYear: "1998" },
+	{ Name: "Eyes Wide Shut", ReleaseYear: "1999" },
+	{ Name: "The Inheritance", ReleaseYear: "1976" }
+]
+
+
+[
+		{"Id":1,"Name":"Fred","Score":88},
+		{"Id":2,"Name":"Kool","Score":45},
+		{"Id":3,"Name":"Ted","Score":78},
+		{"Id":4,"Name":"Annie","Score":14},
+		{"Id":5,"Name":"Susan","Score":67},
+		{"Id":6,"Name":"Jeff","Score":12},
+		{"Id":7,"Name":"Wang","Score":34},
+		{"Id":8,"Name":"Susan","Score":75},
+		{"Id":9,"Name":"Boz","Score":23},
+		{"Id":10,"Name":"Viky","Score":45}
+]
+
+
+jsonp1291062623601({
+		"title": "Recent Uploads tagged cat",
+		"link": "http://www.flickr.com/photos/tags/cat/",
+		"description": "",
+		"modified": "2010-11-29T20:20:13Z",
+		"generator": "http://www.flickr.com/",
+		"items": [
+	   {
+			"title": "DSC05948",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218959764/",
+			"media": {"m":"http://farm6.static.flickr.com/5201/5218959764_ffc2fecc98_m.jpg"},
+			"date_taken": "2010-11-24T10:10:13-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218959764/\" title=\"DSC05948\"><img src=\"http://farm6.static.flickr.com/5201/5218959764_ffc2fecc98_m.jpg\" width=\"240\" height=\"161\" alt=\"DSC05948\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:20:13Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "cat crete"
+	   },
+	   {
+			"title": "Feeding The Cat, Essaouira",
+			"link": "http://www.flickr.com/photos/antonystanley/5218966242/",
+			"media": {"m":"http://farm5.static.flickr.com/4124/5218966242_772e166504_m.jpg"},
+			"date_taken": "2010-11-19T16:04:25-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/antonystanley/\">gripso_banana_prune<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/antonystanley/5218966242/\" title=\"Feeding The Cat, Essaouira\"><img src=\"http://farm5.static.flickr.com/4124/5218966242_772e166504_m.jpg\" width=\"240\" height=\"196\" alt=\"Feeding The Cat, Essaouira\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:22:48Z",
+			"author": "nobody@flickr.com (gripso_banana_prune)",
+			"author_id": "16687586@N00",
+			"tags": "fish cat fisherman fresh morocco catch essaouira"
+	   },
+	   {
+			"title": "DSC05986",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218375245/",
+			"media": {"m":"http://farm6.static.flickr.com/5243/5218375245_967aba1205_m.jpg"},
+			"date_taken": "2010-11-24T14:27:48-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218375245/\" title=\"DSC05986\"><img src=\"http://farm6.static.flickr.com/5243/5218375245_967aba1205_m.jpg\" width=\"160\" height=\"240\" alt=\"DSC05986\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:21:53Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "cat crete"
+	   },
+	   {
+			"title": "DSC05989",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218375857/",
+			"media": {"m":"http://farm6.static.flickr.com/5081/5218375857_e395a8f5d7_m.jpg"},
+			"date_taken": "2010-11-24T14:28:32-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218375857/\" title=\"DSC05989\"><img src=\"http://farm6.static.flickr.com/5081/5218375857_e395a8f5d7_m.jpg\" width=\"240\" height=\"161\" alt=\"DSC05989\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:22:08Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "cat crete"
+	   },
+	   {
+			"title": "DSC05958",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218371875/",
+			"media": {"m":"http://farm6.static.flickr.com/5243/5218371875_2a8c898dda_m.jpg"},
+			"date_taken": "2010-11-24T10:16:57-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218371875/\" title=\"DSC05958\"><img src=\"http://farm6.static.flickr.com/5243/5218371875_2a8c898dda_m.jpg\" width=\"240\" height=\"161\" alt=\"DSC05958\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:20:34Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "cat crete"
+	   },
+	   {
+			"title": "DSC05962",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218961818/",
+			"media": {"m":"http://farm5.static.flickr.com/4132/5218961818_e966f5e205_m.jpg"},
+			"date_taken": "2010-11-24T10:17:25-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218961818/\" title=\"DSC05962\"><img src=\"http://farm5.static.flickr.com/4132/5218961818_e966f5e205_m.jpg\" width=\"240\" height=\"161\" alt=\"DSC05962\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:21:01Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "cat crete"
+	   },
+	   {
+			"title": "Bandit Yawns",
+			"link": "http://www.flickr.com/photos/credmp/5218364467/",
+			"media": {"m":"http://farm5.static.flickr.com/4124/5218364467_7b58d4d07d_m.jpg"},
+			"date_taken": "2010-11-28T19:59:26-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/credmp/\">credmp<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/credmp/5218364467/\" title=\"Bandit Yawns\"><img src=\"http://farm5.static.flickr.com/4124/5218364467_7b58d4d07d_m.jpg\" width=\"240\" height=\"160\" alt=\"Bandit Yawns\" /><\/a><\/p> <p>Bandit shows off his teeth in this mighty yawn!<\/p>",
+			"published": "2010-11-29T20:17:51Z",
+			"author": "nobody@flickr.com (credmp)",
+			"author_id": "31387754@N00",
+			"tags": "cat teeth"
+	   },
+	   {
+			"title": "Predator alarm",
+			"link": "http://www.flickr.com/photos/e3000/5218328929/",
+			"media": {"m":"http://farm6.static.flickr.com/5246/5218328929_5575efe070_m.jpg"},
+			"date_taken": "2008-06-27T17:49:53-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/e3000/\">e³°°°<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/e3000/5218328929/\" title=\"Predator alarm\"><img src=\"http://farm6.static.flickr.com/5246/5218328929_5575efe070_m.jpg\" width=\"240\" height=\"190\" alt=\"Predator alarm\" /><\/a><\/p> <p>We found and raised this disabled sparrow, here on my left arm. But with tree cats-predators (here Fred on the background) we had to be very carefully.<\/p>",
+			"published": "2010-11-29T20:04:27Z",
+			"author": "nobody@flickr.com (e³°°°)",
+			"author_id": "23566085@N00",
+			"tags": "bird danger cat death kat chat arm sparrow mus fred disabled freckles handicap vogel handicapped educating gazon gevaar dangerdemort kreupel fredje freddahousecat"
+	   },
+	   {
+			"title": "DSC05752",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218318905/",
+			"media": {"m":"http://farm6.static.flickr.com/5202/5218318905_1ccaa794f9_m.jpg"},
+			"date_taken": "2010-11-22T11:11:45-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218318905/\" title=\"DSC05752\"><img src=\"http://farm6.static.flickr.com/5202/5218318905_1ccaa794f9_m.jpg\" width=\"240\" height=\"161\" alt=\"DSC05752\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:32Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "cat crete"
+	   },
+	   {
+			"title": "milk glass compote",
+			"link": "http://www.flickr.com/photos/jenscloset/5218319235/",
+			"media": {"m":"http://farm5.static.flickr.com/4087/5218319235_2755e424de_m.jpg"},
+			"date_taken": "2010-11-29T04:18:52-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/jenscloset/\">jenscloset<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/jenscloset/5218319235/\" title=\"milk glass compote\"><img src=\"http://farm5.static.flickr.com/4087/5218319235_2755e424de_m.jpg\" width=\"180\" height=\"240\" alt=\"milk glass compote\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:41Z",
+			"author": "nobody@flickr.com (jenscloset)",
+			"author_id": "26726355@N07",
+			"tags": "blue dog white cute cat vintage puppy ceramic mugs glasses aqua cardinal limegreen adorable casserole ombre retro presents vase rooster ashtray kitschy figurine brightflowers carafe pyrex midcentury christmasgifts barware icebucket goldenfoliage bakeware anchorhocking milkglass soreno delphite elephantbank silvertrimmed"
+	   },
+	   {
+			"title": "angry kitty",
+			"link": "http://www.flickr.com/photos/taiseabel/5218326333/",
+			"media": {"m":"http://farm6.static.flickr.com/5123/5218326333_19fc4e5216_m.jpg"},
+			"date_taken": "2010-11-29T20:03:29-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/taiseabel/\">rathlin taise cecil<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/taiseabel/5218326333/\" title=\"angry kitty\"><img src=\"http://farm6.static.flickr.com/5123/5218326333_19fc4e5216_m.jpg\" width=\"240\" height=\"160\" alt=\"angry kitty\" /><\/a><\/p> <p>one angry angry kitty<\/p>",
+			"published": "2010-11-29T20:03:29Z",
+			"author": "nobody@flickr.com (rathlin taise cecil)",
+			"author_id": "38605623@N07",
+			"tags": "ireland cat scary feline kitty anger angry meow claws mieow"
+	   },
+	   {
+			"title": "Magasin d\'électronique",
+			"link": "http://www.flickr.com/photos/26912359@N07/5218909002/",
+			"media": {"m":"http://farm6.static.flickr.com/5043/5218909002_0909644d1d_m.jpg"},
+			"date_taken": "2010-11-29T12:33:03-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/26912359@N07/\">Slamolo<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/26912359@N07/5218909002/\" title=\"Magasin d\'électronique\"><img src=\"http://farm6.static.flickr.com/5043/5218909002_0909644d1d_m.jpg\" width=\"180\" height=\"240\" alt=\"Magasin d\'électronique\" /><\/a><\/p> <p>Endroit : Télemly (voir la carte pour plus de détails).<\/p>",
+			"published": "2010-11-29T20:01:04Z",
+			"author": "nobody@flickr.com (Slamolo)",
+			"author_id": "26912359@N07",
+			"tags": "street cat kitten chat rue algiers chaton alger"
+	   },
+	   {
+			"title": "DSC05828",
+			"link": "http://www.flickr.com/photos/15482752@N00/5218920208/",
+			"media": {"m":"http://farm5.static.flickr.com/4128/5218920208_143d7a722d_m.jpg"},
+			"date_taken": "2010-11-22T16:00:28-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/15482752@N00/\">Brian-K<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/15482752@N00/5218920208/\" title=\"DSC05828\"><img src=\"http://farm5.static.flickr.com/4128/5218920208_143d7a722d_m.jpg\" width=\"240\" height=\"161\" alt=\"DSC05828\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:05:35Z",
+			"author": "nobody@flickr.com (Brian-K)",
+			"author_id": "15482752@N00",
+			"tags": "dog cat crete"
+	   },
+	   {
+			"title": "Mod bank",
+			"link": "http://www.flickr.com/photos/jenscloset/5218908770/",
+			"media": {"m":"http://farm6.static.flickr.com/5008/5218908770_d196627799_m.jpg"},
+			"date_taken": "2010-11-29T02:59:42-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/jenscloset/\">jenscloset<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/jenscloset/5218908770/\" title=\"Mod bank\"><img src=\"http://farm6.static.flickr.com/5008/5218908770_d196627799_m.jpg\" width=\"240\" height=\"180\" alt=\"Mod bank\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:59Z",
+			"author": "nobody@flickr.com (jenscloset)",
+			"author_id": "26726355@N07",
+			"tags": "blue dog white cute cat vintage puppy ceramic mugs glasses aqua cardinal limegreen adorable casserole ombre retro presents vase rooster ashtray kitschy figurine brightflowers carafe pyrex midcentury christmasgifts barware icebucket goldenfoliage bakeware anchorhocking milkglass soreno delphite elephantbank silvertrimmed"
+	   },
+	   {
+			"title": "Fire King",
+			"link": "http://www.flickr.com/photos/jenscloset/5218319817/",
+			"media": {"m":"http://farm6.static.flickr.com/5003/5218319817_db888cb797_m.jpg"},
+			"date_taken": "2010-11-29T00:22:18-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/jenscloset/\">jenscloset<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/jenscloset/5218319817/\" title=\"Fire King\"><img src=\"http://farm6.static.flickr.com/5003/5218319817_db888cb797_m.jpg\" width=\"240\" height=\"180\" alt=\"Fire King\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:55Z",
+			"author": "nobody@flickr.com (jenscloset)",
+			"author_id": "26726355@N07",
+			"tags": "blue dog white cute cat vintage puppy ceramic mugs glasses aqua cardinal limegreen adorable casserole ombre retro presents vase rooster ashtray kitschy figurine brightflowers carafe pyrex midcentury christmasgifts barware icebucket goldenfoliage bakeware anchorhocking milkglass soreno delphite elephantbank silvertrimmed"
+	   },
+	   {
+			"title": "IMG_2179 cr",
+			"link": "http://www.flickr.com/photos/northernkite/5218918632/",
+			"media": {"m":"http://farm5.static.flickr.com/4090/5218918632_e8cf2e781f_m.jpg"},
+			"date_taken": "2010-11-28T11:00:32-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/northernkite/\">northernkite<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/northernkite/5218918632/\" title=\"IMG_2179 cr\"><img src=\"http://farm5.static.flickr.com/4090/5218918632_e8cf2e781f_m.jpg\" width=\"240\" height=\"148\" alt=\"IMG_2179 cr\" /><\/a><\/p> <p>My cat Ba.Ba. looks to the sky to see where all the snow is coming from. Had two periods of &quot;Thundersnow&quot;, and lightning. The clouds are Cumulonimbus and are so huge the top is at 20,000 feet and an electrical charge is generated from top to bottom of the cloud. A rare occurance and not widely heard due to the damping of sound by the falling snow.<\/p>",
+			"published": "2010-11-29T20:04:58Z",
+			"author": "nobody@flickr.com (northernkite)",
+			"author_id": "48809099@N02",
+			"tags": "cat my"
+	   },
+	   {
+			"title": "josef",
+			"link": "http://www.flickr.com/photos/jenscloset/5218319293/",
+			"media": {"m":"http://farm6.static.flickr.com/5250/5218319293_54343fb562_m.jpg"},
+			"date_taken": "2010-11-29T03:43:29-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/jenscloset/\">jenscloset<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/jenscloset/5218319293/\" title=\"josef\"><img src=\"http://farm6.static.flickr.com/5250/5218319293_54343fb562_m.jpg\" width=\"180\" height=\"240\" alt=\"josef\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:42Z",
+			"author": "nobody@flickr.com (jenscloset)",
+			"author_id": "26726355@N07",
+			"tags": "blue dog white cute cat vintage puppy ceramic mugs glasses aqua cardinal limegreen adorable casserole ombre retro presents vase rooster ashtray kitschy figurine brightflowers carafe pyrex midcentury christmasgifts barware icebucket goldenfoliage bakeware anchorhocking milkglass soreno delphite elephantbank silvertrimmed"
+	   },
+	   {
+			"title": "retro ice bucket",
+			"link": "http://www.flickr.com/photos/jenscloset/5218319555/",
+			"media": {"m":"http://farm5.static.flickr.com/4084/5218319555_5954ef42ac_m.jpg"},
+			"date_taken": "2010-11-29T02:26:30-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/jenscloset/\">jenscloset<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/jenscloset/5218319555/\" title=\"retro ice bucket\"><img src=\"http://farm5.static.flickr.com/4084/5218319555_5954ef42ac_m.jpg\" width=\"180\" height=\"240\" alt=\"retro ice bucket\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:49Z",
+			"author": "nobody@flickr.com (jenscloset)",
+			"author_id": "26726355@N07",
+			"tags": "blue dog white cute cat vintage puppy ceramic mugs glasses aqua cardinal limegreen adorable casserole ombre retro presents vase rooster ashtray kitschy figurine brightflowers carafe pyrex midcentury christmasgifts barware icebucket goldenfoliage bakeware anchorhocking milkglass soreno delphite elephantbank silvertrimmed"
+	   },
+	   {
+			"title": "pretty kitty",
+			"link": "http://www.flickr.com/photos/jenscloset/5218907768/",
+			"media": {"m":"http://farm5.static.flickr.com/4110/5218907768_d451a25ff9_m.jpg"},
+			"date_taken": "2010-11-27T23:31:31-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/jenscloset/\">jenscloset<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/jenscloset/5218907768/\" title=\"pretty kitty\"><img src=\"http://farm5.static.flickr.com/4110/5218907768_d451a25ff9_m.jpg\" width=\"180\" height=\"240\" alt=\"pretty kitty\" /><\/a><\/p> ",
+			"published": "2010-11-29T20:00:38Z",
+			"author": "nobody@flickr.com (jenscloset)",
+			"author_id": "26726355@N07",
+			"tags": "blue dog white cute cat vintage puppy ceramic mugs glasses aqua cardinal limegreen adorable casserole ombre retro presents vase rooster ashtray kitschy figurine brightflowers carafe pyrex midcentury christmasgifts barware icebucket goldenfoliage bakeware anchorhocking milkglass soreno delphite elephantbank silvertrimmed"
+	   },
+	   {
+			"title": "Lit de feuilles mortes",
+			"link": "http://www.flickr.com/photos/26912359@N07/5218907174/",
+			"media": {"m":"http://farm5.static.flickr.com/4126/5218907174_89a881f5a6_m.jpg"},
+			"date_taken": "2010-11-23T12:05:01-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/26912359@N07/\">Slamolo<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/26912359@N07/5218907174/\" title=\"Lit de feuilles mortes\"><img src=\"http://farm5.static.flickr.com/4126/5218907174_89a881f5a6_m.jpg\" width=\"180\" height=\"240\" alt=\"Lit de feuilles mortes\" /><\/a><\/p> <p>Endroit : Fac centrale (voir la carte pour plus de détails).<\/p>",
+			"published": "2010-11-29T20:00:25Z",
+			"author": "nobody@flickr.com (Slamolo)",
+			"author_id": "26912359@N07",
+			"tags": "street cat kitten chat rue algiers chaton alger"
+	   }
+        ]
+})
+
+
